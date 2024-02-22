d52a70530f91f851d249cb4f63e7cbc09ed200f0
Pretty Faith <pfimisuarez@addu.edu.ph>
2024-02-23 00:37:59 +0800

    added cors to share resources

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const toDoListRouter = require('./routes/to-do-list');
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
